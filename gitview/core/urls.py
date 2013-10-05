1db1e6a2fc0d315010f900abde994ebd9918794e
Kevin Brown <bass5098@gmail.com>
2013-10-05 19:34:27 -0400

    Bootstrap time

--- gitview/core/urls.py
+++ gitview/core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = (
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login',
         name="login"),
+    url(r"^logout/$", "django.contrib.auth.views.logout",
+        name="logout"),
 )
