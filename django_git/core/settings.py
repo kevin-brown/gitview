404939c124be2fc302b39b50507462d9b3ac1084
Kevin Brown <bass5098@gmail.com>
2013-10-05 18:01:57 -0400

    Got everything functioning

--- django_git/core/settings.py
+++ django_git/core/settings.py
@@ -2,6 +2,8 @@ import os
 
 AUTH_USER_MODEL = "auth.User"
 
+DEBUG = True
+
 # The secret key should be set through the environment variable
 # "DJANGO_SECRET_KEY".  The default is only here for quick testing and should
 # not be used in a production environment.
@@ -14,7 +16,29 @@ INSTALLED_APPS = (
     "django_git.auth",
     "django_git.repositories",
     "django_git.issues",
+    "django.contrib.admin",
     "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
 )
 
 DATABASES = {
@@ -30,3 +54,12 @@ DATABASES = {
         },
     }
 }
+
+ALLOWED_HOSTS = (
+    "localhost",
+    os.environ.get("DJANGO_HOST", None),
+)
+
+ROOT_URLCONF = "django_git.core.urls"
+
+STATIC_URL = "/static/"
