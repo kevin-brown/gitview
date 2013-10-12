cc0dcc896a2e2b9d97b0973e227ecc7859d78e9e
Kevin Brown <bass5098@gmail.com>
2013-10-12 19:07:15 -0400

    Moved CommitView to the commits submodule

--- gitview/repositories/commits/views.py
+++ gitview/repositories/commits/views.py
@@ -16,3 +16,28 @@ class CommitListView(mixins.RepositoryMixin, mixins.TreeMixin,
         commits = self.git_repository.iter_commits(rev=self.tree_name)
 
         return [Commit(commit, self.repository) for commit in commits]
+
+
+class CommitView(mixins.RepositoryMixin, mixins.CommitsSectionMixin,
+                 TemplateView):
+    template_name = "repositories/commit.html"
+
+    def get_context_data(self, **kwargs):
+        from django.http import Http404
+        from git import BadObject
+        from gitview.repositories.commits.models import Commit
+
+        commit_hash = self.kwargs["commit_hash"]
+        commit = Commit(self.git_repository.commit(commit_hash))
+
+        kwargs["commit"] = commit
+
+        try:
+            previous_commit = self.git_repository.commit("%s~1" % commit_hash)
+            diff = previous_commit.diff(commit.commit, create_patch=True)
+        except BadObject:
+            raise Http404("We can't get the diff for this commit")
+
+        kwargs["commit_diff"] = diff
+
+        return super(CommitView, self).get_context_data(**kwargs)
