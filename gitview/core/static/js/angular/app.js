b39c1f1eb7fe32e58885cd041495a2203ba3a241
Kevin Brown <bass5098@gmail.com>
2013-10-20 20:06:17 -0400

    Force a window reload on unregisted AngularJS routes

--- gitview/core/static/js/angular/app.js
+++ gitview/core/static/js/angular/app.js
@@ -6,27 +6,37 @@ var GitView = {
     repositories: angular.module("gitView.repositories", [])
 }
 
-GitView.app.config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when("/:ownerName/:repositoryName", {
-            templateUrl: "/static/views/repository/index.html",
-            controller: "RepositoryController"
-        })
-        .when("/:ownerName/:repositoryName/tree/:treeName", {
-            templateUrl: "/static/views/repository/index.html",
-            controller: "RepositoryController"
-        })
-        .when("/:ownerName/:repositoryName/tree/:treeName/:treePath*", {
-            templateUrl: "/static/views/repository/index.html",
-            controller: "RepositoryController"
-        })
-        .when("/:ownerName/:repositoryName/commits", {
-            templateUrl: "/static/views/repository/index.html",
-            controller: "CommitListController"
-        })
-        .when("/:ownerName/:repositoryName/commits/:treeName", {
-            templateUrl: "/static/views/repository/index.html",
-            controller: "CommitListController"
-        })
-    $locationProvider.html5Mode(true);
-});
+GitView.app.config([
+    "$routeProvider", "$locationProvider",
+    function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when("/:ownerName/:repositoryName", {
+                templateUrl: "/static/views/repository/index.html",
+                controller: "RepositoryController"
+            })
+            .when("/:ownerName/:repositoryName/tree/:treeName", {
+                templateUrl: "/static/views/repository/index.html",
+                controller: "RepositoryController"
+            })
+            .when("/:ownerName/:repositoryName/tree/:treeName/:treePath*", {
+                templateUrl: "/static/views/repository/index.html",
+                controller: "RepositoryController"
+            })
+            .when("/:ownerName/:repositoryName/commits", {
+                templateUrl: "/static/views/repository/index.html",
+                controller: "CommitListController"
+            })
+            .when("/:ownerName/:repositoryName/commits/:treeName", {
+                templateUrl: "/static/views/repository/index.html",
+                controller: "CommitListController"
+            })
+            .otherwise({
+                resolve: {
+                    wtf: function () {
+                        // Let Django handle unregistered routes
+                        window.location.reload();
+                    }
+                }
+            });
+        $locationProvider.html5Mode(true);
+    }]);
